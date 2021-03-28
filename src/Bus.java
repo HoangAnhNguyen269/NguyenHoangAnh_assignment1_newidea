0268c88aa33d01bfd1befe59cc598e7128da701c
HoangAnhNguyen269 <74226464+HoangAnhNguyen269@users.noreply.github.com>
2021-03-28 22:27:37 +0700

    modify the Bus class from solution

--- src/Bus.java
+++ src/Bus.java
@@ -1,2 +1,8 @@
-public class Bus {
+public class Bus extends Car {
+
+    public Bus(String id, Road currentRoad) {
+        super(id, currentRoad);
+        this.id = ("bus_" + id);
+        length = super.getLength() * 3;//len(bus) = 3 * len(car)
+    }
 }
